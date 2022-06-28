1d7f774964800cce32af73c1a4858391893a71ca
azaymulyalesmana <azay.mulya.lesmana19@mhs.ubharajaya.ac.id>
2022-06-28 13:42:02 +0700

    pembuatan database guru

---
+++ database/migrations/2022_06_28_044749_create_guru.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('guru', function (Blueprint $table) {
+            $table->id();
+            $table->string('nip');
+            $table->string('nama_guru');
+            $table->string('jenis_kelamin');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('alamat');
+            $table->string('no_ktp');
+            $table->string('status_keluarga');
+            $table->string('foto');
+            $table->string('status_pegawai');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('guru');
+    }
+};
