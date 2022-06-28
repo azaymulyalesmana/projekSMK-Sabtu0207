53935ab728d3764db492b4256bd1e0ffb571336d
azaymulyalesmana <azay.mulya.lesmana19@mhs.ubharajaya.ac.id>
2022-06-28 13:39:37 +0700

    pembuatan database create nilai siswa

---
+++ database/migrations/2022_06_28_044921_create_siswa.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nis');
+            $table->string('nama_siswa');
+            $table->string('jenis_kelamin');
+            $table->string('tempat_lahir');
+            $table->date('tanggal_lahir');
+            $table->string('agama');
+            $table->string('alamat');
+            $table->string('no_telp');
+            $table->string('nama_ortu');
+            $table->string('foto');
+            $table->string('pekerjaan_ortu');
+            $table->string('id_kelas');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siswa');
+    }
+};
